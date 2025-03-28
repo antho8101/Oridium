9615c496848ca5e8f3f747facababe85f58866bc
antho8101 <contact@acglobalbusiness.com>
2025-03-28 18:01:17 +0100

    💄 Finalized desktop UI layout for mining panel

--- oridium-wallet/core/Transaction.cpp
+++
@@ -1,9 +0,0 @@
-// Oridium Project - (c) 2025 Oridium - MIT License
-#include "Transaction.h"
-
-Transaction::Transaction(const std::string& sender, const std::string& receiver, double amount)
-    : sender(sender), receiver(receiver), amount(amount), signature("") {}
-
-std::string Transaction::toString() const {
-    return sender + " -> " + receiver + " : " + std::to_string(amount) + " [" + signature + "]";
-}
