5425eac1fb21097e351c1bdd06c00eeba6fb3ae2
antho8101 <contact@acglobalbusiness.com>
2025-04-13 08:29:43 +0200

    Increase Express JSON body limit to 5MB to support block batching

--- oridium-server/server.js
+++ oridium-server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 
 // 🔍 GET /blockchain
 app.get('/blockchain', (req, res) => {
