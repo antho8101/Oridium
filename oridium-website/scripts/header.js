e2f112ee7ceda863f19014c4f53e31ee397e0b19
antho8101 <contact@acglobalbusiness.com>
2025-04-17 18:47:59 +0200

    mobile menu ok

--- oridium-website/scripts/header.js
+++ oridium-website/scripts/header.js
@@ -1,8 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const hamburger = document.getElementById("hamburger-toggle");
-    const mobileMenu = document.getElementById("mobile-menu");
-  
-    hamburger.addEventListener("click", () => {
-      mobileMenu.classList.toggle("open");
+  const burgerButtons = document.querySelectorAll(".burger-menu");
+  const mobileMenu = document.getElementById("mobile-menu");
+
+  if (burgerButtons.length > 0 && mobileMenu) {
+    burgerButtons.forEach(button => {
+      button.addEventListener("click", () => {
+        mobileMenu.classList.toggle("open");
+      });
     });
-  });  
+  }
+});
