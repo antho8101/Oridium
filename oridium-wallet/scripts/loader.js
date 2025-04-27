00a4499c18ab9bcbcf6a05d971ae0172fbd32f0d
antho8101 <contact@acglobalbusiness.com>
2025-04-27 06:25:16 +0200

    let's try something interesting..

--- oridium-wallet/scripts/loader.js
+++
@@ -1,9 +0,0 @@
-export function finishLoading() {
-    const loader = document.getElementById('loader');
-    const app = document.getElementById('app');
-  
-    if (loader && app) {
-      loader.style.display = 'none';
-      app.style.display = 'flex';
-    }
-  }  
